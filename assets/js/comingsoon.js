bb6fc8e69e4ce48795743ef1ef0410c0aceb8ef9
edgeitcie <edgeitcie@gmail.com>
2024-11-03 19:50:08 +0300

    Update comingsoon.js

--- assets/js/comingsoon.js
+++ assets/js/comingsoon.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("Jan 1, 2025 00:00:00").getTime();
+    const countDate = new Date("Nov 27, 2024 00:00:00").getTime();
 
     const currentTime = new Date().getTime();
     console.log(currentTime);
